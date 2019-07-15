af405b16363734a4ed7eeaa9db08970165f93902
chenzhiqian <825863866@qq.com>
2019-07-15 09:41:41 +0800

    chenzhiqian_task_首次提交代码

---
+++ QuartzDotNetDemo-master/QuartzDotNetDemo/QuartzDotNetDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿using Autofac;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Specialized;
+using Topshelf;
+using Topshelf.Autofac;
+
+namespace QuartzDotNetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(config =>
+            {
+                config.SetServiceName(JobService.ServiceName);
+                config.SetDescription("Quartz.NET的demo");
+                config.UseLog4Net();
+                config.UseAutofacContainer(JobService.Container);
+
+                config.Service<JobService>(setting =>
+                {
+                    JobService.InitSchedule(setting);
+                    setting.ConstructUsingAutofacContainer();
+                    setting.WhenStarted(o => o.Start());
+                    setting.WhenStopped(o => o.Stop());
+                });
+            });
+        }
+
+
+        //static void Main(string[] args)
+        //{
+        //    //创建一个调度器工厂
+        //    var props = new NameValueCollection
+        //    {
+        //        { "quartz.scheduler.instanceName", "QuartzDotNetDemo" }
+        //    };
+        //    var factory = new StdSchedulerFactory(props);
+
+        //    //获取调度器
+        //    var sched = factory.GetScheduler();
+        //    sched.Start();
+
+        //    //定义一个任务，关联"HelloJob"
+        //    var job = JobBuilder.Create<HelloJob>()
+        //        .WithIdentity("myJob", "group1")
+        //        .Build();
+
+        //    //由触发器每40秒触发执行一次任务
+        //    var trigger = TriggerBuilder.Create()
+        //        .WithIdentity("myTrigger", "group1")
+        //        .StartNow()
+        //        .WithSimpleSchedule(x => x
+        //            .WithIntervalInSeconds(40)
+        //            .RepeatForever())
+        //        .Build();
+
+        //    sched.ScheduleJob(job, trigger);
+        //}
+    }
+
+    public class HelloJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("你好");
+        }
+    }
+}
---
+++ Sikiro.Nosql.Mongo-master/Sikiro.Nosql.Mongo.Samples/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+using Sikiro.Nosql.Mongo.Base;
+
+namespace Sikiro.Nosql.Mongo.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "mongodb://localhost:27017/";
+            var mongoRepository = new MongoRepository(url);
+
+            var u = new User
+            {
+                Name = "skychen123",
+                BirthDateTime = new DateTime(1992, 2, 2),
+                AddressList = new List<string> { "guangdong", "guangzhou" },
+                Sex = Sex.Son,
+                Son = new User
+                {
+                    Name = "xiaochenpi",
+                    BirthDateTime = DateTime.Now
+                }
+            };
+
+            var addresult = mongoRepository.Add(u);
+
+            //mongoRepository.Update<User>(a => a.Id == u.Id, a => new User
+            //{
+            //    Sex = Sex.Son
+            //});
+
+            //var upResulr = mongoRepository.GetAndUpdate<User>(a => a.Id == u.Id, a => new User { Sex = Sex.Son });
+
+            //var getResult = mongoRepository.Get<User>(a => a.Id == u.Id);
+            //getResult.Name = "superskychen";
+
+            //mongoRepository.Update(getResult);
+
+         
+
+            //mongoRepository.Exists<User>(a => a.Id == u.Id);
+
+            //mongoRepository.Delete<User>(a => a.Id == u.Id);
+
+
+        }
+    }
+
+    [Mongo("chengongtest", "User")]
+    public class User : MongoEntity
+    {
+        public string Name { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime BirthDateTime { get; set; }
+
+        public User Son { get; set; }
+
+        public Sex Sex { get; set; }
+
+        public List<string> AddressList { get; set; }
+    }
+
+    public enum Sex
+    {
+        Woman,
+        Son
+    }
+}
